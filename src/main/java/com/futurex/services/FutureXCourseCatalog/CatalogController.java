dc6612ee9dfb622f5d8682b1afdd05e925408242
Ricardo Medina Sterling <ricardoms1909@gmail.com>
2025-10-20 20:37:31 -0500

    Add application configuration and implement Resilience4J circuit breakers

--- src/main/java/com/futurex/services/FutureXCourseCatalog/CatalogController.java
+++ src/main/java/com/futurex/services/FutureXCourseCatalog/CatalogController.java
@@ -2,6 +2,7 @@ package com.futurex.services.FutureXCourseCatalog;
 
 import com.netflix.appinfo.InstanceInfo;
 import com.netflix.discovery.EurekaClient;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,42 +15,57 @@ public class CatalogController {
     private EurekaClient client;
 
     @RequestMapping("/")
+    @CircuitBreaker(name = "courseService", fallbackMethod ="fallbackGetCatalogHome")
     public String getCatalogHome() {
         String courseAppMesage = "";
         //String courseAppURL = "http://localhost:8080/";
         RestTemplate restTemplate = new RestTemplate();
-        InstanceInfo instanceInfo = client.getNextServerFromEureka("fx-course-service",false);
+        InstanceInfo instanceInfo = client.getNextServerFromEureka("fx-course-service", false);
         String courseAppURL = instanceInfo.getHomePageUrl();
-        courseAppMesage = restTemplate.getForObject(courseAppURL,String.class);
+        courseAppMesage = restTemplate.getForObject(courseAppURL, String.class);
 
-        return("Welcome to FutureX Course Catalog "+courseAppMesage);
+        return ("Welcome to FutureX Course Catalog " + courseAppMesage);
     }
 
     @RequestMapping("/catalog")
+    @CircuitBreaker(name = "courseService", fallbackMethod ="fallbackGetCatalog")
     public String getCatalog() {
         String courses = "";
         //String courseAppURL = "http://localhost:8080/courses";
-        InstanceInfo instanceInfo = client.getNextServerFromEureka("fx-course-service",false);
+        InstanceInfo instanceInfo = client.getNextServerFromEureka("fx-course-service", false);
         String courseAppURL = instanceInfo.getHomePageUrl();
-        courseAppURL = courseAppURL+"/courses";
+        courseAppURL = courseAppURL + "/courses";
         RestTemplate restTemplate = new RestTemplate();
-        courses = restTemplate.getForObject(courseAppURL,String.class);
+        courses = restTemplate.getForObject(courseAppURL, String.class);
 
-        return("Our courses are "+courses);
+        return ("Our courses are " + courses);
     }
 
     @RequestMapping("/firstcourse")
+    @CircuitBreaker(name = "courseService", fallbackMethod ="fallbackGetSpecificCourse")
     public String getSpecificCourse() {
         Course course = new Course();
         //String courseAppURL = "http://localhost:8080/1";
-        InstanceInfo instanceInfo = client.getNextServerFromEureka("fx-course-service",false);
+        InstanceInfo instanceInfo = client.getNextServerFromEureka("fx-course-service", false);
         String courseAppURL = instanceInfo.getHomePageUrl();
-        courseAppURL = courseAppURL+"/1";
+        courseAppURL = courseAppURL + "/1";
         RestTemplate restTemplate = new RestTemplate();
 
-        course = restTemplate.getForObject(courseAppURL,Course.class);
+        course = restTemplate.getForObject(courseAppURL, Course.class);
 
-        return("Our first course is "+course.getCoursename());
+        return ("Our first course is " + course.getCoursename());
+    }
+
+    public String fallbackGetCatalogHome(Exception ex) {
+        return "Fallback: Unable to retrieve course catalog home.";
+    }
+
+    public String fallbackGetCatalog(Exception ex) {
+        return "Fallback: Unable to retrieve course catalog.";
+    }
+
+    public String fallbackGetSpecificCourse(Exception ex) {
+        return "Fallback: Unable to retrieve specific course.";
     }
 
 }
---
+++ src/main/java/com/futurex/services/FutureXCourseCatalog/Resilience4JConfig.java
@@ -0,0 +1,26 @@
+package com.futurex.services.FutureXCourseCatalog;
+
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import io.github.resilience4j.circuitbreaker.CircuitBreakerRegistry;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.Duration;
+
+@Configuration
+public class Resilience4JConfig {
+
+    @Bean
+    public CircuitBreakerConfig circuitBreakerConfig() {
+        return CircuitBreakerConfig.custom()
+                .failureRateThreshold(50)
+                .waitDurationInOpenState(Duration.ofMillis(1000))
+                .slidingWindowSize(2)
+                .build();
+    }
+
+    @Bean
+    public CircuitBreakerRegistry circuitBreakerRegistry(CircuitBreakerConfig circuitBreakerConfig) {
+        return CircuitBreakerRegistry.of(circuitBreakerConfig);
+    }
+}
